6b4f4cea38ec1f562d7dcf7b8692c1fb21f70766
agulujan41 <agulujan41proj@gmail.com>
2024-01-05 21:34:51 -0300

    adding db remote access 5

--- utils/settings.py
+++ utils/settings.py
@@ -130,5 +130,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     "https://marcadorstudents-agulujan-dev.vercel.app",
-    "*"
+    
+    "https://agulujan41.pythonanywhere.com"
 ]
